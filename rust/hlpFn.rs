32f12cf8b995b56e2fbc42e34b9a5390cc81a98e
Julian <julyk.volyansky13@yandex.ru>
2021-04-30 08:15:38 +0300

    Tried to create a dll with rust but no results

--- rust/hlpFn.rs
+++ rust/hlpFn.rs
@@ -1,11 +1,23 @@
 mod hlp_Lib;
 
 use crate::hlp_Lib::*;
+use std::ffi::{CStr, CString};
+
 
 fn main() {
     let arr: [u8; 5] = [0x22, 0x03, 0x34, 0x05, 0x06];
-    let out = hex2ascii(&arr);
 
+    let arr2 = arr.to_vec();
+
+    unsafe {
+        let c_string = CString::from_vec_unchecked(arr2);
+        //let out = hex2ascii(c_string).into_string();
+       // println!("{:?}", out);
+    }
+
+   
+
+    
     
     //println!("{ }", );
     //println!("Hello, world!");
--- rust/hlp_Lib.rs
+++ rust/hlp_Lib.rs
@@ -1,26 +1,29 @@
 use std::ffi::{CStr, CString};
+use std::os::raw::c_uchar;
 
+#[cfg(all(target_os = "win32", target_arch = "x86"))]
+#[link(name = "kernel32")]
 #[no_mangle]
-pub extern "C" fn hex2ascii(hex_in : &[u8]) -> CString{
+pub fn hex2ascii(/*hex_in : CString*/) -> *const u8{
     //let mut ascii_ch : u8 = 0;
     let mut out_str = String::new();
     //let mut ascii_ch = format!("0x{:X?}, ", hex_in[0]);
     //println!("hex2ascii fn {}, {:#X}", hex_len, hex_in[0]);
     
     
-    for element in hex_in.iter()
-    {
-        //ascii_ch = format!("0x{:02X?}, ", element);
-        out_str.push_str(&(format!("0x{:02X?}, ", element)));
-    }
+    // for i in hex_in.to_bytes()
+    // {
+    //     //ascii_ch = format!("0x{:02X?}, ", element);
+    //     out_str.push_str(&(format!("0x{:02X?}, ", i)));
+    // }
 
-    //println!("{ }", out_str.len());
-    out_str.pop();
-    //println!("{ }", out_str.len());
-    out_str.pop();
-    //println!("{ }", out_str);
+    // //println!("{ }", out_str.len());
+    // out_str.pop();
+    // //println!("{ }", out_str.len());
+    // out_str.pop();
+    // //println!("{ }", out_str);
 
-    let s = CString::new(out_str).unwrap();
+    // let s = CString::new(out_str).unwrap();
     
-    return s;
+    return "s".as_ptr();
 }
