ed9956f20562a6fc4cc4df345871a58037491f93
Julian <julyk.volyansky13@yandex.ru>
2021-04-30 04:07:41 +0300

    Created function hex2ascii

--- rust/hlpFn.rs
+++ rust/hlpFn.rs
@@ -1,12 +1,27 @@
-fn hex2ascii(hex_in : &[i8], hex_len: usize){
-    let mut ascii_ch : i8 = 0;
-    ascii_ch = hex_in[0];
-    println!("hex2ascii fn {}, {:#X}", hex_len, hex_in[0]);
+fn hex2ascii(hex_in : &[u8]) -> String{
+    //let mut ascii_ch : u8 = 0;
+    let mut out_str = String::new();
+    //let mut ascii_ch = format!("0x{:X?}, ", hex_in[0]);
+    //println!("hex2ascii fn {}, {:#X}", hex_len, hex_in[0]);
     
+    
+    for element in hex_in.iter()
+    {
+        //ascii_ch = format!("0x{:02X?}, ", element);
+        out_str.push_str(&(format!("0x{:02X?}, ", element)));
+    }
+
+    //println!("{ }", out_str.len());
+    out_str.pop();
+    //println!("{ }", out_str.len());
+    out_str.pop();
+    //println!("{ }", out_str);
+
+    return out_str;
 }
 
 fn main() {
-    let arr: [i8; 5] = [0x22, 0x03, 0x04, 0x05, 0x06];
-    hex2ascii(&arr, arr.len());
+    let arr: [u8; 5] = [0x22, 0x03, 0x34, 0x05, 0x06];
+    println!("{ }", hex2ascii(&arr));
     //println!("Hello, world!");
 }
